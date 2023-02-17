e401b0535d668d2a9a478c694437c03e648ae5e7
Himanshu <himanshu.jaiswal@neosoftmail.com>
2023-02-17 16:07:58 +0530

    Latest update with api

--- App.tsx
+++ App.tsx
@@ -1,46 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import GlovalNavigation from './src/navigators/GlobalNavigation';
 
 function App() {
-
-  // For Normal api call
-  let url = "https://jsonplaceholder.typicode.com"
-  let param = ["data", "users", "1"]
-  if (url) {
-    param.forEach((item) => {
-      url += `/${item}`
-    })
-    console.log('URL', url)
-  }
-  // Normal APi call ends
-
-  useEffect(() => {
-    sendingFunction();
-  })
-  //For api call with Parameters
-  const sendingFunction = () => {
-    let urlDynamic = "https://staging-api.tracknerd.io/v1/auth/login"
-    let params = {
-      usename:'Himanshu',
-      password:'Sunny@1213'
-    }
-    recievingFunction(urlDynamic, params)
-  }
-
-  const recievingFunction = (urlDynamic: any, params: any) => {
-    console.log('params in recieving function', params)
-    console.log('url', url)
-    if (urlDynamic) {
-      let separator = "?";
-      for (const key in params) {
-        urlDynamic += `${separator}${encodeURI(key.toString())}=${encodeURI(params[key].toString())}`
-        separator = "&"
-      }
-    }
-    console.log('UrlDynamic', urlDynamic)
-  }
-
-  //Api call with parameters ends
   return (
     <GlovalNavigation />
   )
